6709017433b4624f37111dc64cc57ada489765e5
Chamoz99 <gimhanichamodika@gmail.com>
2024-02-16 12:29:14 +0530

    imported header and footer

--- src/app/(public)/appointment/book/page.tsx
+++ src/app/(public)/appointment/book/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../../components/navbar/Header";
+import Footer from "../../components/footer/Footer";
+
 
 export default function page() {
   return (
     <div className="absolute w-full text-lg font-semibold text-center top-1/2 bottom-1/2 ">
       appointment book
+    
     </div>
   );
 }
