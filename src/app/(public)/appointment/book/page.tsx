e5469222a16dfc979a7cd2d5780cbdcf450dda35
Chamoz99 <gimhanichamodika@gmail.com>
2024-02-17 23:51:51 +0530

    add changes to page

--- src/app/(public)/appointment/book/page.tsx
+++ src/app/(public)/appointment/book/page.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Header from "../../components/navbar/Header";
 import Footer from "../../components/footer/Footer";
+import "./BookPageStyles.css";
+
 
 
 export default function page() {
   return (
-    <div className="absolute w-full text-lg font-semibold text-center top-1/2 bottom-1/2 ">
-      appointment book
-    
-    </div>
+    <div className="containerFluid">
+      <div className="containerMiddle">
+        <div className="containerText1">
+          Book an Appointment
+        </div>
+        <div className="containerText2dev">
+          <div className="containerText1Inner">Home</div>
+          <div className="text-blue-50 text-base leading-6 uppercase">/</div>
+          <div className="text-white text-base leading-6 uppercase">Appointment</div>
+          <div className="text-blue-50 text-base leading-6 uppercase">/</div>
+          <div className="text-blue-600 text-base leading-6 uppercase">Book An appointment</div>
+        </div>
+      </div>
+  </div>
+  
   );
 }
