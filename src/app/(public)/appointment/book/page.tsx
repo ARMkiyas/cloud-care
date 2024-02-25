493938c5ffada88a4a504fc421f52843eee13a70
A.R.M.Kiyas <62748979+ARMkiyas@users.noreply.github.com>
2024-02-25 18:46:50 +0000

    fix bg problem

--- src/app/(public)/appointment/book/page.tsx
+++ src/app/(public)/appointment/book/page.tsx
@@ -78,7 +78,7 @@ export default function page() {
                 <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
                   <div className="flex flex-col items-stretch w-[24%] max-md:w-full max-md:ml-0">
                     <div className="text-blue-600 text-base font-black leading-4 whitespace-nowrap justify-center items-center bg-white aspect-square w-full h-[55px] px-5 rounded-3xl max-md:mt-10">
-                      
+                        
                     </div>
                   </div>
                   <div className="flex flex-col items-stretch w-[76%] ml-5 max-md:w-full max-md:ml-0">
