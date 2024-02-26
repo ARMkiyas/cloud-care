fe9c512b20d6cd9ebc9cb05d508529de0e5c4c47
Chamoz99 <gimhanichamodika@gmail.com>
2024-02-26 19:15:24 +0530

    Did top main container ui and imported css file from appoinment css file

--- src/app/(public)/doctors/page.tsx
+++ src/app/(public)/doctors/page.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Header from "../../components/navbar/Header";
+import Footer from "../../components/footer/Footer";
+import BookPageStyles from "../../appoinment/book/BookPageStyles";
 
 export default function page() {
   return (
-    <div className="absolute w-full text-lg font-semibold text-center top-1/2 bottom-1/2 ">
-      doctor
+    <div>
+      <div className="mainContainer1">
+        <div className="containerMiddle">
+          <div className="containerText1">
+            Doctors
+          </div>
+          <div className="containerText2dev">
+            <div className="text-white text-base leading-6 uppercase">Home</div>
+            <div className="items-stretch flex justify-between gap-2 pl-2">
+              <div className="text-blue-50 text-base leading-6 uppercase">
+                /
+              </div>
+            </div>
+            <div className="text-blue-600 text-base leading-6 uppercase">
+              Doctors
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
