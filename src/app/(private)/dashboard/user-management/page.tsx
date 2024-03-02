ad4b545100c40588063debb56145018f617ede9d
A.R.M.Kiyas <armkiyas99@hotmail.com>
2024-03-03 03:37:27 +0530

    fixed type error , added data fetching suspence bundary and  optimized some code

--- src/app/(private)/dashboard/user-management/page.tsx
+++ src/app/(private)/dashboard/user-management/page.tsx
@@ -1,25 +1,13 @@
 "use client";
-import React from 'react'
-import TableSort from '@/utils/lib/TableSort/TableSort';
-import { useApiClient } from '@/utils/trpc/Trpc';
+import React from "react";
+
+import { useApiClient } from "@/utils/trpc/Trpc";
+import TableSort from "@/components/TableSort/TableSort";
 
 export default function page() {
-  const {
-    data:userdata,
-    isError,
-    isLoading,
-  }=useApiClient.manageUsers.getUsers.useQuery({});
-  console.log(userdata);
-  
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error fetching data</div>;
-
-  
-
-
   return (
-    <div><TableSort userdata={userdata}/></div>
-  )
+    <div>
+      <TableSort />
+    </div>
+  );
 }
-
