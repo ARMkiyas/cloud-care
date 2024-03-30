6c60de4ad1f4e2a044587c4e349b75d405b5462d
FathimaHumeina <fathimahumaina1@gmail.com>
2024-03-30 23:47:59 +0530

    create public doctor page

--- src/app/(public)/doctors/page.tsx
+++ src/app/(public)/doctors/page.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+"use client";
+import React,{useState} from "react";
 import Header from "../components/navbar/Header";
 import Footer from "../components/footer/Footer";
 import "../appointment/book/page";
 import "./DoctorPageStyles.css";
-import { Pagination } from '@mantine/core';
+import { Pagination,TextInput,Card, Image, Text, Badge, Button, Group} from '@mantine/core';
+import { IconSearch } from "@tabler/icons-react";
+import { useApiClient } from "@/utils/trpc/Trpc";
 
 
 export default function page() {
+  const{data:userdata,isLoading,error}=useApiClient.manageStaff.GetPubDoctors.useQuery({});
+  const [searchQuery, setSearchQuery] = useState('');
+  
+  if (!userdata) return null;
+  // Filter doctors based on search query
+  const filteredDoctors = userdata?.data.filter(doctor => {
+    const { specialization, departments, name } = doctor;
+    return (
+      specialization.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      departments.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  });
+  console.log("userdata",userdata);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  const renderDoctors = filteredDoctors.concat(userdata.data.filter(doctor => !filteredDoctors.includes(doctor)));
+
   return (
     <div>
       <div className="mainContainer1">
@@ -30,156 +52,47 @@ export default function page() {
       <div className="mainBodyContainer">
         <div className="max-w-[1320px] items-center flex flex-col px-3 max-md:max-w-full">
           <div className="max-w-full items-center flex w-[600px] flex-col px-20 max-md:px-5">
-            <div className="text-neutral-400 text-center text-base leading-6 whitespace-nowrap justify-center items-stretch border px-6 py-1.5 rounded-[800px] border-solid border-blue-200 max-md:px-5">
-              Doctors
-            </div>
             <div className="text-blue-950 text-center text-4xl font-bold leading-10 self-stretch mt-4 max-md:max-w-full max-md:mr-2">
               Our Experience Doctors
             </div>
+            <TextInput
+            radius="xl"
+            className="w-full pt-8"
+            placeholder="Search by any field"
+            mb="md"
+            leftSection={<IconSearch />}
+            value={searchQuery}
+              onChange={(event) => setSearchQuery(event.target.value)}
+            />
           </div>
-          <div className="self-stretch mt-12 max-md:max-w-full max-md:mt-10">
-            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-              <div className="flex flex-col items-stretch w-3/12 max-md:w-full max-md:ml-0">
+          <div className="flex space-x-10 flex-wrap justify-center place-items-center " >
+          {renderDoctors.map((doctor,index)=>(
+            <div className="flex flex-col items-stretch w-3/12 max-md:w-full max-md:ml-0 justify-center mt-5" key={index}>
                 <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
                   <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-1.jpg.png"
+                      src={doctor.image}
+                      height={160}
+                     alt={doctor.name}
                     className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
                   />
                   <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
                     <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
+                      {doctor.name}
                     </div>
                     <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
+                      Department: {doctor.departments}
+                    </div>
+                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
+                      Specialization: {doctor.specialization}
+                    </div>
+                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
+                      Total Schedule: {doctor.totalSchedules}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-2.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-3.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-4.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="self-stretch mt-6 max-md:max-w-full">
-            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-              <div className="flex flex-col items-stretch w-3/12 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-2.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-3.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-4.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-stretch w-3/12 ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-stretch self-stretch flex grow flex-col rounded-lg max-md:mt-6">
-                  <img
-                    loading="lazy"
-                    srcSet="/img/doctorPageIMG/team-1.jpg.png"
-                    className="aspect-[0.83] object-contain object-center w-full overflow-hidden"
-                  />
-                  <div className="items-center bg-blue-50 flex flex-col px-20 py-6 max-md:px-5">
-                    <div className="text-blue-950 text-center text-xl font-bold leading-6 whitespace-nowrap">
-                      Doctor Name
-                    </div>
-                    <div className="text-blue-600 text-center text-base leading-6 whitespace-nowrap mt-2">
-                      Department
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+           
+          ))}
           </div>
         </div>
         <div className="flex items-start gap-3.5 mr-2.5 mt-9 self-end">
