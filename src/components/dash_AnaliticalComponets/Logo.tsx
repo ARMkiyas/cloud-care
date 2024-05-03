d4ded55b1db3041d518df26a73a942a148450ac7
A.R.M.Kiyas <62748979+ARMkiyas@users.noreply.github.com>
2024-05-03 20:05:13 +0000

    fixed bugs for prod

--- src/components/dash_AnaliticalComponets/Logo.tsx
+++ src/components/dash_AnaliticalComponets/Logo.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
 
-
 export function Logo(props: React.ComponentPropsWithoutRef<typeof Image>) {
-    return (
-      <Image {...props}  src="http://localhost:3000/logo.png" alt="Mantine logo"  />
-    );
-  }
+  return <Image {...props} src="/logo.png" alt="Mantine logo" />;
+}
